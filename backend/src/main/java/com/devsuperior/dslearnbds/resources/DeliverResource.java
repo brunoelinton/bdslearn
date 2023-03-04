a585142eeb0d7c0700ba426266acc962424cbb51
brunoelinton <brunoelinton@outlook.com>
2023-03-04 20:45:54 -0300

    Backend: Preauthorizing methods

--- backend/src/main/java/com/devsuperior/dslearnbds/resources/DeliverResource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/DeliverResource.java
@@ -1,8 +1,14 @@
 package com.devsuperior.dslearnbds.resources;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslearnbds.entities.dtos.DeliverRevisionDTO;
 import com.devsuperior.dslearnbds.services.DeliverService;
 
 @RestController
@@ -15,5 +21,10 @@ public class DeliverResource {
 		this.deliverService = deliverService;
 	}
 	
-
+	@PreAuthorize("hasAnyRole('ADMIN', 'INSTRUCTOR')")
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> saveRevision(@PathVariable Long id, @RequestBody DeliverRevisionDTO dto) {
+        deliverService.saveRevision(id, dto);
+        return ResponseEntity.noContent().build();
+    }
 }
